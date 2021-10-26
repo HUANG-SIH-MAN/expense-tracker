0fbae374db5a1b8ec5d558632704ad5517c283e8
HUANG-SIH-MAN <cindy20303705@gmail.com>
2021-10-26 23:57:43 +0800

    feat:新增類別搜尋功能，想新增handlebars helper判別支出類別(失敗暫時註解)

--- app.js
+++ app.js
@@ -2,11 +2,26 @@ const express = require('express')
 const exphbs = require('express-handlebars')
 const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
+const category = require('./models/category')
 const routes = require('./routers')
 const app = express()
 const port = 3000
 
-app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
+app.engine('handlebars', exphbs({ defaultLayout: 'main'
+    // ,helpers: {
+    //     getCategoryIcon: function (categoryId) {
+    //        return category.findById(categoryId)
+    //        .lean()
+    //        .then(item => item.icon)
+    //     }
+    // ,helpers: {
+    //     getCategoryIcon: async function (categoryId) {
+    //         const item = await category.findById(categoryId).lean()
+    //        return item.icon
+    //     }
+    // }
+}))
+
 app.set('view engine', 'handlebars')
 app.use(express.static('public'))
 app.use(express.urlencoded({ extended: true }))
--- routers/modules/home.js
+++ routers/modules/home.js
@@ -1,15 +1,27 @@
 const express = require('express')
 const router = express.Router()
 const expense = require('../../models/expense')
+const category = require('../../models/category')
 
-router.get('/',(req, res)=>{
-    expense.find()
+router.get('/', async (req, res)=>{
+    const categoryData = await category.find().select('name').lean() 
+    const { expenses, totalAmount } = await expense.find()
     .lean()
     .sort({ date: 'desc' }) 
     .then(expenses =>{   
         const totalAmount = expenses.map(expense => expense.amount).reduce((a, b) => a + b)
-        res.render('index', { expenses, totalAmount })
+        return {expenses, totalAmount}
     })
+    res.render('index', { expenses, totalAmount, categoryData })
+})
+
+router.post('/search', async (req, res) => {
+    const { categoryId } = req.body
+    const categoryData = await category.find().select('name').lean()
+    const searchResult = await expense.find({ categoryId }).lean()
+    return res.render('index', { expenses: searchResult, categoryData})
 })
 
 module.exports = router
+
+//res.render('index', { expenses, totalAmount })
