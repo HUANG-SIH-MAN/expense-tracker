414867ea99577d6ea13c27eb890bb21dc72d23d6
HUANG-SIH-MAN <cindy20303705@gmail.com>
2021-10-27 10:24:02 +0800

    feat:完成登入與註冊頁面

--- routers/index.js
+++ routers/index.js
@@ -3,7 +3,9 @@ const router = express.Router()
 
 const home = require('./modules/home')
 const expense = require('./modules/expense')
+const user = require('./modules/user')
 
+router.use('/users', user)
 router.use('/expense', expense)
 router.use('/', home)
 
