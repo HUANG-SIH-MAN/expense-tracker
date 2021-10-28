f6619d8869776c33f49e7f56974bd393a201e993
HUANG-SIH-MAN <cindy20303705@gmail.com>
2021-10-28 09:51:13 +0800

    feat:新增FB登入功能

--- config/passport.js
+++ config/passport.js
@@ -1,6 +1,7 @@
 const passport = require('passport')
 const bcrypt = require('bcryptjs')
 const LocalStrategy = require('passport-local').Strategy
+const FacebookStrategy = require('passport-facebook')
 const User = require('../models/user')
 
 module.exports = app => {
@@ -22,6 +23,25 @@ module.exports = app => {
         })
         .catch(err => done(err, false))
     }))
+
+    //Facebook 登入
+    passport.use(new FacebookStrategy({  
+        clientID: process.env.FACEBOOK_ID,
+        clientSecret: process.env.FACEBOOK_SECRET,
+        callbackURL: process.env.FACEBOOK_CALLBACK,
+        profileFields: ['email', 'displayName']
+    }, (accessToken, refreshToken, profile, done)  => {
+        const { name, email } = profile._json
+        User.findOne({email})
+        .then(async (user) => {
+            if (user) return done(null, user)
+            const randomPassword = Math.random().toString(36).slice(-8)
+            const hashPassword = await bcrypt.genSalt(10).then(salt => bcrypt.hash(randomPassword, salt))
+            await User.create({ name, email, password: hashPassword }).then(()=> done(null, user))
+        })
+        .catch(err => done(err, false))
+    }))
+                                          
 
     //序列化/反序列化
     passport.serializeUser((user, done) => {
--- routers/index.js
+++ routers/index.js
@@ -4,9 +4,11 @@ const router = express.Router()
 const home = require('./modules/home')
 const expense = require('./modules/expense')
 const user = require('./modules/user')
+const auth = require('./modules/auth')
 const { authenticator } = require('../middleware/auth')
 
 router.use('/users', user)
+router.use('/auth', auth)
 router.use('/expense', authenticator, expense)
 router.use('/', authenticator, home)
 
