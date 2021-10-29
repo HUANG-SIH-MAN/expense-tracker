01b193c9abe923c06e618c27ffae5f7e72e967ce
HUANG-SIH-MAN <cindy20303705@gmail.com>
2021-10-30 00:39:14 +0800

    feat:將首頁與搜尋路由合併

--- routers/modules/home.js
+++ routers/modules/home.js
@@ -4,39 +4,17 @@ const router = express.Router()
 const expense = require('../../models/expense')
 const category = require('../../models/category')
 
-router.get('/', async (req, res)=>{
-    const userId = req.user._id
-    //取得分類名稱，用於select選單
-    const categoryData = await category.find().select('name').lean() 
-    //取得支出資料，並計算總金額
-    const { expenses, totalAmount } = await expense.find({ userId })
-    .lean()
-    .sort({ date: 'desc' }) 
-    .then(expenses =>{ 
-        if (expenses.length === 0) return res.render('index',{ categoryData })  
-        const totalAmount = expenses.map(expense => expense.amount).reduce((a, b) => a + b)
-        //取得類別圖示和時間轉換
-        expenses.forEach(item => {
-            category.findById(item.categoryId).then(category => item.icon = category.icon)
-            item.date = moment(item.date).format('YYYY-MM-DD')
-        })
-        return { expenses, totalAmount }
-    })
-    res.render('index', { expenses, totalAmount, categoryData })
-})
-
-router.post('/search', async (req, res) => {
+router.get('/', async (req, res) => {
     const userId = req.user._id
     //取得類別相關資料
-    const { categoryId } = req.body
+    const { categoryId } = req.query
     const categoryData = await category.find().select('name').lean()
 
-    // const searchCaregory = category.findById(categoryId).then(item => item.name)
-    // categoryData.forEach(item => item.searchCaregory = searchCaregory)
+    //判定是使用是否使用搜尋功能
+    const expenseFind = categoryId ? { $and: [{userId}, {categoryId}] } : { userId }
 
-    // console.log(categoryData)
     //取得支出相關資料
-    const { searchResult, totalAmount } = await expense.find({ $and: [{userId}, {categoryId}] })
+    const { searchResult, totalAmount } = await expense.find(expenseFind)
     .lean()
     .sort({ date: 'desc' }) 
     .then(searchResult =>{
@@ -55,3 +33,8 @@ router.post('/search', async (req, res) => {
 
 module.exports = router
 
+
+    // const searchCaregory = category.findById(categoryId).then(item => item.name)
+    // categoryData.forEach(item => item.searchCaregory = searchCaregory)
+
+    // console.log(categoryData)
